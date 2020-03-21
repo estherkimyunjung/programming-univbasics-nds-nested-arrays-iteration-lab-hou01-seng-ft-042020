cf4b1af90113c710b723ca69b44c4cf51c50da5b
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-21 22:01:24 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -36,7 +36,10 @@ def join_ingredients(src)
   while i < src.length do
     inner = src[i]
     result << "I love #{inner[0]} and #{inner[1]} on my pizza"
-    i += 1
+    i++
+  end
+  result
+  
 end
 
 
