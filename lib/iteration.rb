ade0053dc441b48049058ba786a3be53fd247f33
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-21 22:08:52 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -75,7 +75,7 @@ def find_greater_pair(src)
   i = 0
   
   while i < src.length do
-    result = (src[i][0] > src[i][1] ? src[i][0] : src[i][1])
+    result.push(src[i][0] > src[i][1] ? src[i][0] : src[i][1])
     i += 1
   end  
   result
